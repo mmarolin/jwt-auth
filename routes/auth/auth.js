f34a57a414acea239757cda7755098cc902342b3
mmarolin <matija.marolin11@gmail.com>
2022-04-25 17:02:52 +0200

    creating login endpoint

--- routes/auth/auth.js
+++ routes/auth/auth.js
@@ -1,7 +1,8 @@
 const router = require("express").Router();
 const jwt = require("jsonwebtoken");
 const Joi = require("@hapi/joi");
-const user = require("../../models/user");
+const users = require("../../models/users");
+const bcrypt = require("bcryptjs");
 
 const registerSchema = Joi.object({
   fname: Joi.string().min(3).required(),
@@ -10,19 +11,24 @@ const registerSchema = Joi.object({
   password: Joi.string().min(8).required(),
 });
 
+//REGISTER
+
 router.post("/register", async (req, res) => {
-  const emailExist = await user.findOne({ email: req.body.email });
+  const emailExist = await users.findOne({ email: req.body.email });
 
   if (emailExist) {
     res.status(400).send("Email already in use.");
     return;
   }
 
-  const User = new user({
+  const salt = await bcrypt.genSalt(10);
+  const hashedPass = await bcrypt.hash(req.body.password, salt);
+
+  const user = new users({
     fname: req.body.fname,
     lname: req.body.lname,
     email: req.body.email,
-    password: req.body.password,
+    password: hashedPass,
   });
 
   try {
@@ -32,8 +38,35 @@ router.post("/register", async (req, res) => {
       res.status(400).send(error.details[0].message);
       return;
     } else {
-      const saveUser = await User.save();
+      const saveUser = await user.save();
       res.status(200).send("user created");
+    }
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
+const loginSchema = Joi.object({
+  email: Joi.string().min(8).required().email(),
+  password: Joi.string().min(6).required(),
+});
+
+//LOGIN
+
+router.post("/login", async (req, res) => {
+  const user = await users.findOne({ email: req.body.email });
+
+  if (!user) return res.status(400).send("Incorrect Email");
+
+  const validPass = await bcrypt.compare(req.body.password, user.password);
+
+  if (!validPass) return res.status(400).send("Incorrect Password");
+
+  try {
+    const { err } = await loginSchema.validateAsync(req.body);
+    if (err) return res.status(400).send(err.details[0].message);
+    else {
+      res.send("Success!");
     }
   } catch (err) {
     res.status(500).send(err);
